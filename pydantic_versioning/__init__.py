bd92c3320405b04c4f983e650d58962416e62d31
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-07-10 16:33:20 +0200

    Initial commit

---
+++ pydantic_versioning/__init__.py
@@ -0,0 +1,10 @@
+__package__ = "pydantic_versioning"
+__author__ = "Mohamed Ragaey Saleh"
+__license__ = "MIT"
+__version__ = "0.2.0"
+__maintainer__ = "Mohamed Ragaey Saleh"
+__email__ = "mohamed.ragaiy.saleh@gmail.com"
+__status__ = "Development"
+
+
+__all__ = ()
